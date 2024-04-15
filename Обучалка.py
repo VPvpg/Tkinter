a30ce33d72ba801861a64e171194495be871f700
VPvpg <109874895+VPvpg@users.noreply.github.com>
2024-04-15 12:34:15 +0300

    Update Обучалка.py

--- Обучалка.py
+++ Обучалка.py
@@ -4,3 +4,5 @@ window = tk.Tk()
 window.title("Надпись в заголовке окна.")
 
 window.geometry("400x300+300+200") #размер окна и его позиция при выскакивании
+
+window.mainloop() #запуск "функции" https://poe.com/s/rPt45fvebUDW6HjsJuaO
